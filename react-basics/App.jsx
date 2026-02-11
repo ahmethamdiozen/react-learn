34036ca5a908594cc98b69e80b1f9616e7dbb2c4
ahmethamdiozen <hamdiahmetozen@gmail.com>
2026-02-11 18:31:20 +0300

    react-basics generated

---
+++ react-basics/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+function App() {
+    return (
+        <body>
+            <Navbar />
+            <Main />
+        </body>
+    )
+}
+export default App
---
+++ react-basics/components/Main.jsx
@@ -0,0 +1,7 @@
+function Main() {
+    return (
+        <h1>MAIN</h1>
+    ) 
+}
+
+export default Main
