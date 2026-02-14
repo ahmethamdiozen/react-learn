2d5a449d407d42634e0ee36f7c97bdd41c2bd8db
ahmethamdiozen <hamdiahmetozen@gmail.com>
2026-02-14 22:56:10 +0300

    destructuring practise

---
+++ my-react-app/src/destructureExport.jsx
@@ -0,0 +1,10 @@
+const person = {
+    physical: {
+        height: 180,
+        weight: 90
+    },
+    iq: 140,
+    name: "hamdi"
+}
+
+export default person
---
+++ my-react-app/src/destructureImport.jsx
@@ -0,0 +1,13 @@
+import person from "./destructureExport"
+
+// const { physical } = person
+// console.log(physical) // {height: 180, weight: 90}
+
+// const { height } = person.physical
+// console.log(height) // 180
+
+
+// const { physical } = person.physical
+// console.log(physical) // undefined
+
+
