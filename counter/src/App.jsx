5959b2fbc786f65cfd5d37fea4d6d8135fb9dba1
ahmethamdiozen <hamdiahmetozen@gmail.com>
2026-02-15 23:49:38 +0300

    previous state usage on useState

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,20 +4,26 @@ export default function App() {
     const [count, setCount] = React.useState(0)
     
     function add() {
-        setCount(count + 1)
+        setCount(prevCount => prevCount + 1)
     }
     
     function subtract() {
-        setCount(count - 1)
+        setCount(prevCount => prevCount - 1)
     }    
     
     return (
         <main className="container">
             <h1>Counter on React</h1>
             <div className="counter">
-                <button className="minus" onClick={subtract} aria-label="Decrease count">–</button>
+                <button 
+                  className="minus" 
+                  onClick={subtract} 
+                  aria-label="Decrease count">–</button>
                 <h2 className="count">{count}</h2>
-                <button className="plus" onClick={add} aria-label="Increase count">+</button>
+                <button 
+                  className="plus" 
+                  onClick={add} 
+                  aria-label="Increase count">+</button>
             </div>
         </main>
     )
