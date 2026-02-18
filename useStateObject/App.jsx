2a773c6fd8c26553488cc0123be45b292cb62a46
ahmethamdiozen <hamdiahmetozen@gmail.com>
2026-02-18 23:38:18 +0300

    setting state from child component

--- useStateObject/App.jsx
+++ useStateObject/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import avatar from "./images/user.png"
-import starFilled from "./images/star-filled.png"
-import starEmpty from "./images/star-empty.png"
+import Star from "./Star"
 
 export default function App() {
     const [contact, setContact] = React.useState({
@@ -9,10 +8,8 @@ export default function App() {
         lastName: "Doe",
         phone: "+1 (212) 555-1212",
         email: "itsmyrealname@example.com",
-        isFavorite: false
+        isFavorite: true
     })
-    
-    let starIcon = contact.isFavorite ? starFilled : starEmpty
 
     function toggleFavorite() {
         setContact(prevContact => ({
@@ -20,7 +17,7 @@ export default function App() {
             isFavorite: !prevContact.isFavorite
         }))
     }
-
+    
     return (
         <main>
             <article className="card">
@@ -30,18 +27,7 @@ export default function App() {
                     alt="User profile picture of John Doe"
                 />
                 <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
+                    <Star isFilled={contact.isFavorite} />
                     <h2 className="name">
                         {contact.firstName} {contact.lastName}
                     </h2>
